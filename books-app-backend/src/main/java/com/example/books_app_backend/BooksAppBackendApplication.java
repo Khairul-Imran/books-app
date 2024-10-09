547322c4f7000d1defb6c7a098906e5de9aeb4da
Khairul <Khairulimran6810@gmail.com>
2024-10-09 21:19:21 +0800

    First commit.

---
+++ books-app-backend/src/main/java/com/example/books_app_backend/BooksAppBackendApplication.java
@@ -0,0 +1,13 @@
+package com.example.books_app_backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BooksAppBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BooksAppBackendApplication.class, args);
+	}
+
+}
---
+++ books-app-backend/src/test/java/com/example/books_app_backend/BooksAppBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.books_app_backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BooksAppBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
